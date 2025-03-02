931cf131c5916984fd2134597d32e2011513083c
myd7349 <myd7349@gmail.com>
2025-03-02 12:06:28 +0800

    Update musl toolchains

--- toolchains/aarch64-linux-musl-cross.cmake
+++ toolchains/aarch64-linux-musl-cross.cmake
@@ -2,13 +2,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(BASE_DIR "${CMAKE_CURRENT_LIST_DIR}/../aarch64-linux-musl-cross/bin")
-
-set(CMAKE_C_COMPILER "${BASE_DIR}/aarch64-linux-musl-gcc")
-set(CMAKE_CXX_COMPILER "${BASE_DIR}/aarch64-linux-musl-g++")
-set(CMAKE_AR "${BASE_DIR}/aarch64-linux-musl-ar")
-set(CMAKE_STRIP "${BASE_DIR}/aarch64-linux-musl-strip")
-set(CMAKE_RANLIB "${BASE_DIR}/aarch64-linux-musl-ranlib")
+set(CMAKE_C_COMPILER aarch64-linux-musl-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-musl-g++)
+set(CMAKE_AR aarch64-linux-musl-ar)
+set(CMAKE_STRIP aarch64-linux-musl-strip)
+set(CMAKE_RANLIB aarch64-linux-musl-ranlib)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
--- toolchains/x86_64-linux-musl-cross.cmake
+++ toolchains/x86_64-linux-musl-cross.cmake
@@ -2,13 +2,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(BASE_DIR "${CMAKE_CURRENT_LIST_DIR}/../x86_64-linux-musl-cross/bin")
-
-set(CMAKE_C_COMPILER "${BASE_DIR}/x86_64-linux-musl-gcc")
-set(CMAKE_CXX_COMPILER "${BASE_DIR}/x86_64-linux-musl-g++")
-set(CMAKE_AR "${BASE_DIR}/x86_64-linux-musl-ar")
-set(CMAKE_STRIP "${BASE_DIR}/x86_64-linux-musl-strip")
-set(CMAKE_RANLIB "${BASE_DIR}/x86_64-linux-musl-ranlib")
+set(CMAKE_C_COMPILER x86_64-linux-musl-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
+set(CMAKE_AR x86_64-linux-musl-ar)
+set(CMAKE_STRIP x86_64-linux-musl-strip)
+set(CMAKE_RANLIB x86_64-linux-musl-ranlib)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
